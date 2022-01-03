50de8d59b79e98fc41c49c2c085a6197e1644a8f
Tim Egenton <22758764+tegenton@users.noreply.github.com>
2022-01-02 20:38:10 -0500

    individual test status

--- unit.c
+++ unit.c
@@ -2,8 +2,16 @@
 #include "tests.h"
 int main(){
     unsigned int failed_tests = 0;
+    int failure;
     for (size_t i=0; unit_tests[i]!=NULL; i++){
-        failed_tests += unit_tests[i]();
+        failure = unit_tests[i]();
+        printf("TEST #%d - ", i);
+        if (failure) {
+            printf("FAILED\n");
+        } else {
+            printf("PASSED\n");
+        }
+        failed_tests += failure;
     }
     if (failed_tests){
         printf("FAILED %u TESTS.\n", failed_tests);
